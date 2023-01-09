ab6e264ef3663c05a3931fbab5a81314dac592d7
sergey-frd <sergey.frd@gmail.com>
2023-01-09 17:42:47 +0200

    230109
    230109

--- !r_ae_System_1.bat
+++ !r_ae_System_1.bat
@@ -1,4 +1,8 @@
 c:
 cd c:\Program Files\Adobe\Adobe After Effects 2023\Support Files\
 rem AfterFX   -noui -r  "e:\\Git\\P_h_o_t_o\\AE_JSX\\AE_Matvey\\sf_System_22_s2.jsx"
-AfterFX   -noui -r  "e:\\Git\\P_h_o_t_o\\AE_JSX\\AE_Matvey\\sf_System_22_s5b.jsx"
+rem AfterFX   -noui -r  "e:\\Git\\P_h_o_t_o\\AE_JSX\\AE_Matvey\\sf_System_22_s5b.jsx"
+rem AfterFX   -noui -r  "e:\\Git\\P_h_o_t_o\\AE_JSX\\AE_Matvey\\sf_System_22_s5m.jsx"
+rem AfterFX   -noui -r  "e:\\Git\\P_h_o_t_o\\AE_JSX\\AE_Matvey\\sf_System_22_s5mm.jsx"
+rem AfterFX   -noui -r  "e:\\Git\\P_h_o_t_o\\AE_JSX\\AE_Matvey\\sf_System_22_s5m3.jsx"
+AfterFX   -noui -r  "e:\\Git\\P_h_o_t_o\\AE_JSX\\AE_Matvey\\sf_System_22_s5m4.jsx"
